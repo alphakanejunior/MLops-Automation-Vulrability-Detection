7b321862f89823cc69f32a8661eff9ec78f07ae5
Alpha Kane <alphakanejunior10@gmail.com>
2026-01-21 03:11:44 -0500

    add version final

--- scripts/cve_enrichment.py
+++ scripts/cve_enrichment.py
@@ -177,7 +177,7 @@ final_report = {
 # ==========================================================
 # Affichage console
 # ==========================================================
-print("\n")
+print("\n ******************** Rapport_code_vulns ********************")
 print("\n🔐 CODE VULNERABILITIES")
 print(tabulate(
     [[v["file"], v["line"], v.get("mapped_cve",""), v.get("nvd_severity",""), v["issue"]] for v in code_vulns],
@@ -185,7 +185,7 @@ print(tabulate(
     tablefmt="github"
 ))
 
-print("\n")
+print("\n ******************** Rapport_dependency_vulns ********************")
 print("\n📦 DEPENDENCY VULNERABILITIES")
 print(tabulate(
     [[v["package"], v["version"], v["cve"], v["severity"], (v["description"] or "")[:120]] for v in dependency_vulns],
@@ -194,7 +194,7 @@ print(tabulate(
 ))
 
 if model_vulns:
-    print("\n")
+    print("\n ******************** Rapport_model_vulns ********************")
     print("\n🧪 MODEL VULNERABILITIES")
     print(tabulate(
         [[v["file"], v["description"], v["severity"], v["tool"]] for v in model_vulns],
@@ -203,7 +203,7 @@ if model_vulns:
     ))
 
 if container_vulns:
-    print("\n")
+    print("\n ******************** Rapport_container_vulns ********************")
     print("\n🐳 CONTAINER VULNERABILITIES")
     print(tabulate(
         [[v["target"], v["package"], v["version"], v["vulnerability_id"], v["severity"], (v["description"] or "")[:120]] for v in container_vulns],
